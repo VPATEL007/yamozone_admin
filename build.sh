a6b53a48b66287b6760e58b6acfe7a80e5ea7cba
Vijaykumar Laheri <92792941+VPATEL007@users.noreply.github.com>
2026-04-02 21:28:48 +0530

    update flutter version

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+cat > build.sh << 'EOF'
+#!/bin/bash
 set -e
 
 rm -rf $HOME/flutter
 rm -rf /vercel/path0/flutter
 
-curl -o /tmp/flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.2-stable.tar.xz
+curl -o /tmp/flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.4-stable.tar.xz
 tar xf /tmp/flutter.tar.xz -C $HOME
 
 export PATH="$HOME/flutter/bin:$PATH"
